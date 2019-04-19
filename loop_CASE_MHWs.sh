d446c2ea6614440d44868b718903f4db196e6d16
Eugene Noh <blackcata109@gmail.com>
2019-04-19 18:11:11 +0900

    Modify the temporal file

--- loop_CASE_MHWs.sh
+++ loop_CASE_MHWs.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 #------------------------------------------------------------------------------------#
-#                                                                                    #
-#                  SCRIPT for loop for specific cases to each ncl file               #
-#                                                                                    #
-#                                                                 BY   :  KM.Noh     #
-#                                                                 DATE : 2019.04.19  #
-#                                                                                    #
+#										                                             #
+#			     SCRIPT for loop for specific cases to each ncl file  	             #
+#										                                             #
+#								                                 BY   :  KM.Noh      #
+#								                                 DATE : 2019.04.19   #
+#							   			                                             #
 #------------------------------------------------------------------------------------#
 
 # Specific cases are designed with specific lat, lon range
@@ -15,13 +15,13 @@ export case_end=9   # Last  case of MHWs
 
 # Loop for each cases ncl file
 for case in $(seq $case_str $case_end); do 
-    export file_name="Scat_MHWs_index_CHL_SST.ncl"
+    export file_name="Scat_MHWs_index_MLD_SST.ncl"
     export line_change="s/region_case/"$case"/"
     
     # Change the case and make temporal file
-    sed $line_change $file_name > tmp.ncl 
+    sed $line_change $file_name > tmp_$file_name 
 
-    #Execute ncl file and remove file
-    ncl tmp.ncl 
-    rm tmp.ncl
+    #Excute ncl file and remove file
+    ncl tmp_$file_name
+    rm tmp_$file_name
 done
