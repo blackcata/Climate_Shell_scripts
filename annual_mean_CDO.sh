751fdda6727e8cee4a07d8fa27d80e902ed83b45
blackcata <blackcata109@gmail.com>
2023-03-31 22:31:04 +0900

    Add converting monthly data to yearly data using CDO

---
+++ annual_mean_CDO.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#------------------------------------------------------------------------------------#
+#					 				             #      
+#	                    SCRIPT FOR ANNUAL MEANING OF VARIABLES		             #
+#								   BY   : KM.NOH        #
+#				 				   DATE : 2022.11.17 #      
+#								                     #      
+#------------------------------------------------------------------------------------#
+
+export path_input="/data1/CMIP6/SSP585/monthly/dissic/regrid/vert_regrid"
+export path_output="/home/km109/Analysis/Python/Arctic_Carbon_Cycle/DATA/CMIP6_SSP585_DIC"
+export list=$(ls ${path_input})
+
+# Start convert monthly data to yearly data
+for input in $list; do
+#export input="3d_regrid_360x180_dissic_Omon_GFDL-ESM4_1pctCO2-cdr_r1i1p1f1_gr_014101-034012.nc"
+    # Set input & output files for regridding
+    export file_input=$path_input"/"$input
+    export file_output=$path_output"/ann_"$input
+
+    echo ""
+    echo "!-----------------------------------------------------------------------!"
+    echo "INPUT : "$file_input
+    echo "OUTPUT : "$file_output
+    echo ""
+
+    # Excute the annual mean by using CDO commands
+    cdo -yearmean $file_input $file_output
+
+    echo "ANNUAL MEAN COMPLETED "
+    echo "!-----------------------------------------------------------------------!"
+    echo ""
+done
---
+++ annual_season_mean_CDO.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#------------------------------------------------------------------------------------#
+#					 				             #      
+#	                    SCRIPT FOR ANNUAL MEANING OF VARIABLES IN SEASON		#
+#								   BY   : KM.NOH        #
+#				 				   DATE : 2023.03.31 #      
+#								                     #      
+#------------------------------------------------------------------------------------#
+
+export path_input="/data1/CMIP6/SSP585/monthly/dissic/regrid/vert_regrid"
+export path_output="/home/km109/Analysis/Python/Arctic_Carbon_Cycle/DATA/CMIP6_SSP585_DIC"
+export list=$(ls ${path_input})
+export season_name="SON"
+
+# Start convert monthly data to yearly data
+for input in $list; do
+#export input="ta_Amon_NorESM2-MM_historical_r1i1p1f1_gn_201001-201412.nc"
+    # Set input & output files for regridding
+    export file_input=$path_input"/"$input
+    export file_output=$path_output"/ann_"$season_name"_"$input
+
+    echo ""
+    echo "!-----------------------------------------------------------------------!"
+    echo "INPUT : "$file_input
+    echo "OUTPUT : "$file_output
+    echo ""
+
+    # Excute the annual mean in specific season by using CDO commands
+    cdo -seasmean -selseason,$season_name $file_input $file_output
+
+    echo "ANNUAL MEAN ("$season_name") COMPLETED "
+    echo "!-----------------------------------------------------------------------!"
+    echo ""
+done
